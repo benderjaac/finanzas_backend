d477f7f9c116a9147e88b99da4645f6bb1fe740a
Angel Arango <arangocruzjoseangel@gmail.com>
2025-06-03 19:45:54 -0600

    cambios atrasados de jwtuserdetails

--- src/main/java/com/primeng/primeng/services/JwtUserDetailsService.java
+++ src/main/java/com/primeng/primeng/services/JwtUserDetailsService.java
@@ -1,16 +1,19 @@
 package com.primeng.primeng.services;
 
+import com.primeng.primeng.models.Permiso;
 import com.primeng.primeng.models.User;
 import com.primeng.primeng.repositories.UserRepository;
 import com.primeng.primeng.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
@@ -19,14 +22,20 @@ public class JwtUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(()-> new UsernameNotFoundException("Usuario no encontrado"));
+        User user = userRepository.findByUsernameWithPermisos(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        List<GrantedAuthority> authorities = user.getPerfil().getPermisos().stream()
+                .map(Permiso::getRol)
+                .filter(rol -> rol != null && !rol.isBlank())
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
 
         return new CustomUserDetails(
                 user.getId(),
                 user.getUsername(),
                 user.getPassword(),
-                List.of() // o tus roles/permisos aquí
+                authorities // o tus roles/permisos aquí
         );
     }
 }
